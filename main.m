660941acdd1eaba809c1dc41816b3f76e4ef973e
Valentin Débénath <valentin.debenath@gmail.com>
2018-05-11 13:43:19 +0200

    Transformation de Fourier à court terme

--- main.m
+++ main.m
@@ -1,6 +1,9 @@
-xo = rand(1,fe*4);%%Signal de parole
+xo = rand(1,fe*10);%%Signal de parole
+
+%Découpage du signal de parole
+
 space = 4000;
-x = [zeros(1,space),xo];
+x = xo;%[zeros(1,space),xo];
 
 %%T < 125ms (durée max d'un phonème)
 %%T > 20 ms (voix basse d'un homme : 50hz)
@@ -21,29 +24,36 @@ s=q*Te;
 dn = N/2;
 dt = dn*Te;
  
-Mmax = floor(length(x)*dt);
+Mmax = floor(length(x)/dn)-1;
 
-u = zeros(Mmax,length(q));
+u = zeros(Mmax+1,length(q));
 
-for m = 2:Mmax
-    t=m*dt;
+for m = 0:Mmax-1
     for qv = -N/2:N/2-1
-        if m*dn+qv<Mmax
-            u(m,qv+N/2+1) = x(m*dn+qv)*w(qv+N/2+1);
-        end
+        u(m+1,qv+N/2+1) = x(m*dn+qv+N/2+1)*w(qv+N/2+1);
     end
 end
-%%Traitement à faire sur u je suppose ?
-%%....
+%%Transformation de Fourier à court terme
+
+X = zeros(Mmax+1,N);
+
+for m = 0:Mmax-1
+   uf = u(m+1,1:N);
+   UF = abs(fft(uf));
+   for k = 1:N
+        X(m+1,k) = UF(k);
+   end
+end
+
 
 %%Reconstruction
 x2 = zeros(1,length(x));
 
-for n=1:length(x)
+for n=1:length(x)-2*dn
     m = floor(n/dn);
     qv = n-m*dn;
     %fprintf('qv : %d qv-dn : %d\n',qv+dn,qv-dn);
     x2(n) = u(m+1,qv+dn+1)+u(m+1+1,qv+1);
 end
-x2 = x-x2;
-xf = x2(space+1:length(x2));
+%x2 = x-x2;
+xf = x2;%(space+1:length(x2));
