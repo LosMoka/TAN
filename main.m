698e2f616f19fd69a4e9076e20451b7e7ce9792f
NathanBompart <nathan.bompart@telecomnancy.net>
2018-05-13 20:55:57 +0200

    Add files via upload
    ajout cepstre

--- main.m
+++ main.m
@@ -1,3 +1,4 @@
+fe = 44100; %%freq d'éch d'un CD-ROM
 xo = rand(1,fe*10);%%Signal de parole
 
 %Découpage du signal de parole
@@ -7,7 +8,7 @@ x = xo;%[zeros(1,space),xo];
 
 %%T < 125ms (durée max d'un phonème)
 %%T > 20 ms (voix basse d'un homme : 50hz)
-fe = 44100; %%freq d'éch d'un CD-ROM
+
 Te = 1/fe;
 %%T doit respecter T = 2^N / fe, N entier
 %%T doit être trouvé expérimentalement en respectant les contrainte
@@ -45,6 +46,47 @@ for m = 0:Mmax-1
    end
 end
 
+%calcul du cepstre court terme du signal de parole
+c=zeros(Mmax+1,N);
+for m = 0:Mmax-1
+   tmp = X(m+1,1:N);
+   idft= ifft(log(abs(tmp)));
+   for k = 1:N
+        c(m+1,k) = idft(k);
+   end
+end
+
+%fenetrage du cepstre avec jseuil à trouver experimentalement
+
+jseuil=0;
+
+cprime=zeros(Mmax+1,N);
+for j=jseuil:Mmax-jseuil-1
+    for k = 1:N
+        cprime(j+1,k) = c(j+1,k);
+    end
+end
+
+%calcul du logarithme de la reponse frequentielle
+Cprime=zeros(Mmax+1,N);
+for m=0:Mmax-1
+    cm = cprime(m+1,1:N);
+   CM = abs(fft(cm));
+   for k = 1:N
+        Cprime(m+1,k) = CM(k);
+   end
+end
+
+%Calcul de l'estimée de la réponse fréquentielle
+H=zeros(Mmax+1,N);
+for m=0:Mmax-1
+   hm = Cprime(m+1,1:N);
+   HM = abs(fft(cm));
+   for k = 1:N
+        H(m+1,k) = exp(HM(k));
+   end
+end
+
 
 %%Reconstruction
 x2 = zeros(1,length(x));
@@ -57,3 +99,9 @@ for n=1:length(x)-2*dn
 end
 %x2 = x-x2;
 xf = x2;%(space+1:length(x2));
+
+
+
+
+
+
